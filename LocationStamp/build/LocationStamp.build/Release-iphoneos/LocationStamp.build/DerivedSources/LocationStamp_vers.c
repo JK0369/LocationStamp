07bfd0feb43a299cd909c56e9578c0a8dc48c1ff
김종권 <jongkwon.kim@42dot.com>
2020-12-29 02:29:04 +0900

    .

--- LocationStamp/build/LocationStamp.build/Release-iphoneos/LocationStamp.build/DerivedSources/LocationStamp_vers.c
+++ LocationStamp/build/LocationStamp.build/Release-iphoneos/LocationStamp.build/DerivedSources/LocationStamp_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char LocationStampVersionString[];
  extern const double LocationStampVersionNumber;
 
- const unsigned char LocationStampVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LocationStamp  PROJECT:LocationStamp-11" "\n";
- const double LocationStampVersionNumber __attribute__ ((used)) = (double)11.;
+ const unsigned char LocationStampVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LocationStamp  PROJECT:LocationStamp-12" "\n";
+ const double LocationStampVersionNumber __attribute__ ((used)) = (double)12.;
