f518cef89e81758ea38e7dccb928c5248eb5c297
김종권 <jongkwon.kim@42dot.com>
2020-12-29 13:43:34 +0900

    .

---
+++ Domain/build/Domain.build/Release-iphoneos/Domain.build/DerivedSources/Domain_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char DomainVersionString[];
+ extern const double DomainVersionNumber;
+
+ const unsigned char DomainVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Domain  PROJECT:Domain-1" "\n";
+ const double DomainVersionNumber __attribute__ ((used)) = (double)1.;
