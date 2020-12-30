9132f8f9251c6733769c740765e57f5a894ae2bc
김종권 <jongkwon.kim@42dot.com>
2020-12-30 22:39:16 +0900

    .

--- LocationStamp/LocationStamp/ViewController.swift
+++ LocationStamp/LocationStamp/ViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
+enum Person {
+    case name(String)
+    case age(Int)
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let person: Person = .name("이름")
+
+        //
+switch person {
+case .name(let nameValue):
+    print(nameValue)
+case .age(let ageValue):
+    print(ageValue)
+}
+
+        //
+if case let Person.name(nameValue) = person {
+    print(nameValue)
+}
+
+guard case let Person.name(nameValue) = person else {
+    print("not exist")
+    return
+}
+print(nameValue)
     }
 
 
